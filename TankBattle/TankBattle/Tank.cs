05ee1e592d7bf16608e3bb9c812691217cc93f00
bill shan <swbuild@qq.com>
2018-01-06 22:57:12 +0800

    第一次上传，新建了一个空项目，搭建完git环境

---
+++ TankBattle/TankBattle/Tank.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankBattle
+{
+    /// <summary>
+    /// 坦克类
+    /// </summary>
+    class Tank
+    {
+    }
+}
